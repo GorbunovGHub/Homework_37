a56ff6081fd659e79b07766b40b3d7c5cbc2fcd5
GorbunovGHub <pavlixrus@mail.ru>
2024-07-07 14:50:53 +0300

    Add files via upload

---
+++ Homework_37.py
@@ -0,0 +1,66 @@
+from threading import Thread
+import queue
+import time
+
+
+class Table:
+
+    def __init__(self, number):
+        self.number = number
+        self.is_busy = False
+
+
+class Cafe:
+
+    def __init__(self, tables):
+        self.queue = queue.Queue()
+        self.tables = tables
+
+    def customer_arrival(self):
+        customer_number = 1
+        for i in range(20):
+            print(f'Посетитель номер {customer_number} прибыл.')
+            customer_thread = Customer(customer_number, self)
+            customer_thread.start()
+            customer_number += 1
+            time.sleep(1)
+
+    def serve_customer(self, customer):
+        expect_table = False
+        for table in self.tables:
+            if not table.is_busy:
+                table.is_busy = True
+                print(f'Посетитель номер {customer.number} сел за стол № {table.number}.')
+                time.sleep(5)
+                table.is_busy = False
+                print(f'Посетитель номер {customer.number} покушал и ушёл!.')
+                expect_table = True
+                break
+
+        if not expect_table:
+            print(f'Посетитель номер {customer.number} ожидает свободный стол.')
+            self.queue.put(customer)
+            self.queue.get()
+
+
+class Customer(Thread):
+    def __init__(self, number, cafe):
+        super().__init__()
+        self.number = number
+        self.cafe = cafe
+
+    def run(self):
+        self.cafe.serve_customer(self)
+
+
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
+
+cafe = Cafe(tables)
+
+customer_arrival_thread = Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
+
+customer_arrival_thread.join()
